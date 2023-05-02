efc10cea243185f2cb38d9c49a23f6695b0b638d
aazyabin <azyabon9@gmail.com>
2023-05-02 21:17:48 +0700

    init

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>
+          Openai Chat
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
